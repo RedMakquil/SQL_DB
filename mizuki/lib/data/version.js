7de8c7dc4d67ad404ebe2c7b890b27da9aaadb7e
✬  DINUKA HIMSARA✬ <93124334+dinukahimsara1@users.noreply.github.com>
2021-11-19 08:46:08 +0530

    Update version.js

--- mizuki/lib/data/version.js
+++ mizuki/lib/data/version.js
@@ -2,7 +2,7 @@ const Axios = require("axios")
 const exec = require('child_process').exec;
 const os = require("os");
 async function versioninfo(version) {
-    var sdn = 'RUN git clone https://github.com/MrJoka-Thejaka/Mizuki /root/QueenMizuki' + '\n'
+    var sdn = 'RUN git clone https://github.com/dinukahimsara1/Mizuki /root/QueenMizuki' + '\n'
     exec('sed -n 3p /root/QueenMizuki/Mizuki/Dockerfile', async (err, stdout, stderr) => {
       if (sdn !== stdout) {
         throw new Error("Fake - Unknown Device !!");
@@ -16,7 +16,7 @@ async function versioninfo(version) {
     versiontype_si: '',
     versiondata_si: ''
   }
-  await Axios.get('https://gist.githubusercontent.com/sisula/06dc80809b5b7fa41d284c25bc47f4e8/raw/').then(async (response) => {
+  await Axios.get('https://gist.githubusercontent.com/dinukahimsara1/a4817eb1cb1cfb4c3dd662583a09ca9c/raw/00a468c1fc9243a8a699d5ded4356fc518c7746d/announsment.json').then(async (response) => {
     data.versionenglish = response.data.result.version.versionen
     data.versiontype_en = response.data.result.version.en_update_type
     data.versiondata_en = response.data.result.version.en_update_data
